7b1f237bf5f46e954a2f0e16a8766208a7dea21d
Zirani Jean-Luc <jlzirani@gmail.com>
2013-11-11 21:18:24 +0100

    [mod] mod join methodes with almost 1 parametersw

--- ircbot/defaultMod.py
+++ ircbot/defaultMod.py
@@ -52,6 +52,11 @@ class defaultMod(botMod):
 		self.bot.sendMsg(ev.target, "I have the following commands: "+ ', '.join(set(self.bot.dirMsg.keys()+ self.bot.pubMsg.keys() + self.bot.privMsg.keys())))
 		self.bot.sendMsg(ev.target, "Have a nice chat and get the bot with you !")
 
-
 	def join(self, ev, cmd):
-		map(self.bot.connection.join, re.findall(r'#[a-zA-Z0-9_]{2,9}', cmd[1]))
+		if len(cmd) > 1:
+			map(self.bot.connection.join, re.findall(r'#[a-zA-Z0-9_]{2,9}', cmd[1]))
+
+
+
+
+
